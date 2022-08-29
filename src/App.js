c4806b159cc6f1314a5f27f5738bc1796d92b3fb
nordcho <fed270@gmail.com>
2022-08-29 23:05:14 +0300

    add authorization

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Filters from './components/Filters';
 import Products from './components/Products';
-import {createContext, useRef} from 'react';
+import React, {createContext, useEffect, useRef, useState} from 'react';
 
-export const ProductContext = createContext(null)
+export const UserContext = createContext(null)
 
 function App() {
 
 const AppRef = useRef(null)
 
+const [isAuth, setAuth] = useState(false)
+
+// useEffect(() => {
+//   let cookie = document.cookie.split('; ').find(cookie => cookie.startsWith('react-shop')).split('=')[1]
+//   if(cookie == 'qwerty') {
+//     setAuth(true)
+//   }
+//   else {
+//     setAuth(false)
+//   }
+// }, [])
+
    return (
     <div ref={AppRef} className='light'>
-        <Header AppRef={AppRef}/>
+      <UserContext.Provider value={isAuth}>
+        <Header AppRef={AppRef} isAuth={isAuth} setAuth={setAuth}/>
         <Main>
           <Filters />
           <Products/>
         </Main>
+      </UserContext.Provider>
     </div>
   );
 }
